d6f2f5c002b3ba52a490c23a29f314987ae4efad
Salle79 <salle79@gmail.com>
2015-01-06 20:00:58 +0100

    rtyretret

---
+++ WebApi2Book.Common/DateTimeAdapter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace WebApi2Book.Common
+{
+    public class DateTimeAdapter : IDateTime
+    {
+
+        #region IDateTime Members
+
+        public DateTime UtcNow
+        {
+            get { return DateTime.UtcNow; }
+        }
+
+        #endregion
+
+
+
+    }
+}
---
+++ WebApi2Book.Common/Logging/LogManagerAdapter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using log4net;
+namespace WebApi2Book.Common.Logging
+{
+    public class LogManagerAdapter : ILogManager 
+    {
+        #region ILogManager Members
+
+        public log4net.ILog GetLog ( Type typeAssociatedWithRequestedLog )
+        {
+            return LogManager.GetLogger (typeAssociatedWithRequestedLog );
+        }
+
+        #endregion
+    }
+}
